4eda04b9b86f0647db467b29d0217003b4cea4c8
moises <mo.sepulvedar@gmail.com>
2022-12-27 23:39:09 -0300

    home consulting checkpoint

--- porfolio/consulting/models.py
+++ porfolio/consulting/models.py
@@ -8,8 +8,28 @@ from wagtail.models import Page
 class HomeConsulting(Page):
     hero_title = models.CharField(max_length=50, verbose_name='Título principal', default='Título')
 
+    hero_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        verbose_name='Imagen principal',
+    )
+
     content_panels = Page.content_panels + [
         MultiFieldPanel([
             FieldPanel('hero_title'),
-        ], heading='Sección principal')
+            FieldPanel('hero_image'),
+        ], heading='Sección principal'),
+    ]
+
+
+class ServiceConsultingPage(Page):
+    description = models.TextField()
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel([
+            FieldPanel('description')
+        ])
     ]
