6b3c16bfdb4572f22de006645d356e135ac11e3f
moises <mo.sepulvedar@gmail.com>
2022-12-28 23:31:23 -0300

    service page adjustments

--- porfolio/consulting/models.py
+++ porfolio/consulting/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 # Create your models here.
+from wagtail.admin import blocks
 from wagtail.admin.panels import MultiFieldPanel, FieldPanel
+from wagtail.fields import StreamField
+from wagtail.images.blocks import ImageChooserBlock
 from wagtail.models import Page
 
 
@@ -26,10 +29,55 @@ class HomeConsulting(Page):
 
 
 class ServiceConsultingPage(Page):
-    description = models.TextField()
+    hero_title = models.CharField(max_length=50, null=True, blank=False,
+                                  verbose_name='Título del servicio')
+
+    hero_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        verbose_name='Imagen principal',
+    )
+
+    body = StreamField([
+        ('h1', blocks.CharBlock(form_classname="title")),
+        ('h2', blocks.CharBlock(form_classname="title")),
+        ('h3', blocks.CharBlock(form_classname="title")),
+        ('h4', blocks.CharBlock(form_classname="title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('quote', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+        ('youtube_video', blocks.URLBlock()),
+        ('gallery', blocks.ListBlock(ImageChooserBlock(), icon='image', label='Gallery')),
+    ], use_json_field=True, verbose_name='Descripción del servicio', null=True, blank=False)
+
+    side_block_title = models.CharField(max_length=30,
+                                        verbose_name='Título bloque del costado',
+                                        default='Otros servicios')
+
+    contact_us_title = models.CharField(max_length=30,
+                                        verbose_name='Título de contactanos',
+                                        default='¿Tienes alguna duda?')
 
     content_panels = Page.content_panels + [
         MultiFieldPanel([
-            FieldPanel('description')
-        ])
+            FieldPanel('hero_title'),
+            FieldPanel('hero_image'),
+        ], heading='Sección principal'),
+        FieldPanel('body'),
+        MultiFieldPanel([
+            FieldPanel('side_block_title'),
+            FieldPanel('contact_us_title'),
+        ], heading='información del costado'),
     ]
+
+    def get_context(self, request):
+        # Filter by tag
+        other_services = ServiceConsultingPage.objects.exclude(pk=self.pk).all()[:5]
+
+        # Update template context
+        context = super().get_context(request)
+        context['other_services'] = other_services
+        return context
