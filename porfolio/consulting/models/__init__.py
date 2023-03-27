4ada23f2656d2218297ba2bf5d17dafd171f8283
moises <mo.sepulvedar@gmail.com>
2023-03-26 23:00:42 -0300

    header and footer separation

--- porfolio/consulting/models/__init__.py
+++ porfolio/consulting/models/__init__.py
@@ -2,3 +2,4 @@ from .info_page import InfoPage
 from .home_consulting import HomeConsulting
 from .service_consulting_page import ServiceConsultingPage
 from .form_page import FormPage
+from .footer_snippet import Footer
