c5c7be65843548536931a08b0c1ccd7f4081c384
moises <mo.sepulvedar@gmail.com>
2023-03-29 01:13:15 -0300

    database fixing

--- porfolio/porfolio/settings/dev.py
+++ porfolio/porfolio/settings/dev.py
@@ -21,7 +21,12 @@ MIDDLEWARE = MIDDLEWARE + [
 
 INTERNAL_IPS = ("127.0.0.1", "192.168.101.12", 'localhost')
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 def show_toolbar(request):
--- porfolio/porfolio/settings/production.py
+++ porfolio/porfolio/settings/production.py
@@ -4,6 +4,17 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASS'],
+        'HOST': os.environ['DB_SERVICE'],
+        'PORT': os.environ['DB_PORT']
+    }
+}
+
 try:
     from .local import *
 except ImportError:
